72ba0b559dd644782df629d2cf82e694fefd37a6
Kyle Escobar <kslrtips@gmail.com>
2020-10-07 22:36:28 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "org.spectral"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
